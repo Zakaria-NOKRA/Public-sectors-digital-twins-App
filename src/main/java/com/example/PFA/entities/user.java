6d42ff83f2e53e507b0b3f69dab614a17f751b67
BalerioGT <elafouibadr@gmail.com>
2024-05-29 14:25:36 +0100

    Add User class

--- src/main/java/com/example/PFA/entities/user.java
+++ src/main/java/com/example/PFA/entities/user.java
@@ -1,5 +1,23 @@
 package com.example.PFA.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
 public class user {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private String id;
+    private String name;
+    private String surname;
+    private String phone;
+    private String cin;
 }
