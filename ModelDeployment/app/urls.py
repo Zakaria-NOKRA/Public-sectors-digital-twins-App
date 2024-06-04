55b0a62ed901240776862b74a8dc52fe352b1a23
NOKRA Zakaria <zakaria.nokra.ensias@gmail.com>
2024-06-04 13:12:26 +0100

    model deployment ...

--- ModelDeployment/app/urls.py
+++ ModelDeployment/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('detection',views.objectDetection, name='detection')
+    path('detection',views.objectDetection, name='detection'),
+    path('prediction', views.carddetection, name='prediction')
 ]
--- ModelDeployment/app/views.py
+++ ModelDeployment/app/views.py
@@ -2,9 +2,13 @@
 import easyocr
 import cv2
 import re
+import json
+from json import JSONEncoder
 from django.http import request
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from ultralytics import YOLO
+
 
 reader = easyocr.Reader(["en"])
 
@@ -48,6 +52,44 @@ def responseShaping(result):
             dates.append(text)
     
     return {"cin": cins, "name": list(names), "date": dates}
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+@csrf_exempt
+def carddetection(request):
+
+    if request.method == 'POST' and request.FILES.get('img1'):
+        img1 = request.FILES['img1']
+        # Save the uploaded image to a temporary location
+        with open('temp.jpg', 'wb') as f:
+            for chunk in img1.chunks():
+                f.write(chunk)
+        # Read the saved image using OpenCV
+        img = cv2.imread('temp.jpg')
+    
+        model = YOLO(r'C:/Users/GF63/OneDrive/Bureau/PFA/ModelDeployment/SavedModel/best.pt')
+
+        results = model.predict(source=img)
+
+        boxes = results[0].boxes.xyxy.cpu().tolist()
+        for box in boxes:
+            crop_obj = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+        
+
+        d = {"results": crop_obj}
+
+        encodedNumpyData = json.dumps(d, cls=NumpyArrayEncoder)
+
+        return JsonResponse(encodedNumpyData)
+
+
+
 
     
     
